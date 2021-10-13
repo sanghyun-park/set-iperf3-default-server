595dd1d3f8071376c5f43255b1608533fd2e333f
Sanghyun Park <drrrnino7@ddongbbang.com>
2021-10-13 10:48:37 +0900

    Update setup.sh
    Add start script download

--- setup.sh
+++ setup.sh
@@ -8,3 +8,7 @@ net.ipv4.tcp_rmem = 4096 87380 4194304
 net.ipv4.tcp_wmem = 4096 87380 4194304' | sudo tee /etc/sysctl.conf
 
 sudo sysctl -p
+
+rm -f start_iperf_server.sh
+curl https://raw.githubusercontent.com/sanghyun-park/set-iperf3-default-server/main/start_iperf_server.sh > start_iperf_server.sh
+chmod +x start_iperf_server.sh
