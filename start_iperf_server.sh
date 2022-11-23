4096bdc0763f9fbe12b0fea2fda4391582f65c37
Sanghyun Park <drrrnino7@ddongbbang.com>
2022-11-23 10:44:40 +0900

    Add ts pipe to prepend timestamp

--- start_iperf_server.sh
+++ start_iperf_server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo killall iperf3
+sudo killall iperf3 > /dev/null 2>&1
 
 LOGDATE=`date +%Y%m%d-%H%M%S`
-nohup iperf3 -s >> ./iperf3-$LOGDATE.log 2>&1 &
+nohup iperf3 -s | ts '[%Y-%m-%d %H:%M:%S]' >> ./iperf3-$LOGDATE.log 2>&1 &
